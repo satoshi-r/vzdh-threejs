946d0b3ac20ed15f441739117026a39d471e093d
satoshi-r <rgeargerge@yandex.ru>
2020-03-10 18:44:02 +0300

    stop animation on resize

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
 import divide from './divide.js'
 
 console.log(divide(6, 2))
+
+// Остановить анимацию во время изменения размера окна
+let resizeTimer;
+window.addEventListener("resize", () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+});
